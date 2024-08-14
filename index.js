b8d989d534de83baa29a0d4d0da31d0c81324f86
ashwinoodhorah <ashwinoodhorah17@gmail.com>
2024-08-14 19:59:55 +0400

    Commit initial files

---
+++ index.js
@@ -0,0 +1,42 @@
+let homeCount = 0
+let guestCount = 0
+let homeScoreEl = document.getElementById("home-score-el")
+let guestScoreEl = document.getElementById("guest-score-el")
+// console.log(guestScoreEl)
+
+function addHomeOne() {
+    homeCount +=1
+    // console.log(count)
+    homeScoreEl.textContent = homeCount
+}
+
+function addHomeTwo() {
+    homeCount +=2
+    homeScoreEl.textContent = homeCount
+}
+
+function addHomeThree() {
+    homeCount +=3
+    homeScoreEl.textContent = homeCount
+}
+
+function addGuestOne() {
+    guestCount +=1
+    // console.log(count)
+    guestScoreEl.textContent = guestCount
+}
+
+function addGuestTwo() {
+    guestCount +=2
+    guestScoreEl.textContent = guestCount
+}
+
+function addGuestThree() {
+    guestCount +=3
+    guestScoreEl.textContent = guestCount
+}
+
+function startNewGame() {
+    homeScoreEl.textContent = 0
+    guestScoreEl.textContent = 0
+}

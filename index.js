7d7a921e8e3f0758e56fada1860bc4fcb89e7cc5
ashwinoodhorah <ashwinoodhorah17@gmail.com>
2024-08-18 10:37:17 +0400

    handled new game btn

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
-let homeCount = 0
-let guestCount = 0
-let homeScoreEl = document.getElementById("home-score-el")
-let guestScoreEl = document.getElementById("guest-score-el")
+let homeCount = 0;
+let guestCount = 0;
+let homeScoreEl = document.getElementById("home-score-el");
+let guestScoreEl = document.getElementById("guest-score-el");
 // console.log(guestScoreEl)
 
 function addHomeOne() {
-    homeCount +=1
-    // console.log(count)
-    homeScoreEl.textContent = homeCount
+  homeCount += 1;
+  // console.log(count)
+  homeScoreEl.textContent = homeCount;
 }
 
 function addHomeTwo() {
-    homeCount +=2
-    homeScoreEl.textContent = homeCount
+  homeCount += 2;
+  homeScoreEl.textContent = homeCount;
 }
 
 function addHomeThree() {
-    homeCount +=3
-    homeScoreEl.textContent = homeCount
+  homeCount += 3;
+  homeScoreEl.textContent = homeCount;
 }
 
 function addGuestOne() {
-    guestCount +=1
-    // console.log(count)
-    guestScoreEl.textContent = guestCount
+  guestCount += 1;
+  // console.log(count)
+  guestScoreEl.textContent = guestCount;
 }
 
 function addGuestTwo() {
-    guestCount +=2
-    guestScoreEl.textContent = guestCount
+  guestCount += 2;
+  guestScoreEl.textContent = guestCount;
 }
 
 function addGuestThree() {
-    guestCount +=3
-    guestScoreEl.textContent = guestCount
+  guestCount += 3;
+  guestScoreEl.textContent = guestCount;
 }
 
 function startNewGame() {
-    homeScoreEl.textContent = 0
-    guestScoreEl.textContent = 0
+  homeCount = 0;
+  guestCount = 0;
+  homeScoreEl.textContent = 0;
+  guestScoreEl.textContent = 0;
 }
